41a769f29efcf962f80d59fa37349398d691cea4
inwoo <inwoox@naver.com>
2020-11-23 12:51:46 +0900

    재사용을 위한 Vue 컴포넌트에서, 더 작게 쪼갠 Vue 컴포넌트를 import하여 사용하기

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,12 +1,17 @@
 
 // ES6 모듈 (컴포넌트)
 // 컴포넌트를 정의하는 options 객체를 기본 내보내기로 내보낸다.
+import MessageListItem from './MessageListItem.js';
 export default {
   name: 'MessageList',
+  components: {
+    MessageListItem
+  },
+  // :item은 이 컴포넌트에서, MessageListItem 컴포넌트로 props을 전달하는 것
+  // :key는 리스트에서 어떤 아이템이 변경되었는지 추적해, DOM에서 해당 부분만 업데이트하도록, 리스트에서 아이템의 고유 아이디가 될 수 있는 키를 지정
   template: `<ul>
-  <li v-for="item in items" :item="item">
-  {{ item.text }} - {{ item.createdAt }}
-  <button @click="deleteMessage(item)">X</button></li></ul>`,
+  <message-list-item v-for="item in items" :item="item" :key="item.id" @delete="deleteMessage(item)">
+  </message-list-item></ul>`,
   props: {
     items: {
       type: Array,
---
+++ components/MessageListItem.js
@@ -0,0 +1,17 @@
+
+export default {
+  name: 'MessageListItem',
+  template: `<li> {{ item.text }} - {{ item.createdAt }}
+  <button @click="deleteClicked">X</button></li>`,
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    deleteClicked() {
+      this.$emit('delete');
+    }
+  }
+}

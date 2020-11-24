10fecf761b82963b1272dbab6229f72c80aaeb19
inwoo <inwoox@naver.com>
2020-11-24 11:17:34 +0900

    타임리프 의존성 추가 (spring-boot-starter-thymeleaf) / 컨트롤러가 welcome 뷰를 반환하도록 변경 / welcome 뷰 페이지 생성 / welcome 뷰에 타임리프 문법 사용

--- backend/src/main/java/app/messages/MessageController.java
+++ backend/src/main/java/app/messages/MessageController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/messages")
 public class MessageController {
   @GetMapping("/welcome")
-  @ResponseBody // 이거 없이 @Controller만 @RestController로 바꿔도 동일, 반환 값을 뷰가 아니라 HTTP 응답으로
-  public String welcome() {
-    return "Hello, Welcome to Spring Boot!";
+  public String welcome(Model model) {
+    model.addAttribute("message", "Hello, Spring Boot!!"); // Model을 통해, 반환되는 welcome 뷰에 속성을 전달
+    return "welcome";
   }
 }

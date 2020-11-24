82f95f4c49127f977cd148066b994c4c57d49d50
seco <inwoox@gmail.com>
2020-11-24 20:45:57 +0900

    자바 EE 필터 사용하기 / 경과 시간을 계산하고 INFO 로그로 출력하는 필터 생성

--- backend/src/main/java/app/messages/AppConfig.java
+++ backend/src/main/java/app/messages/AppConfig.java
@@ -1,11 +1,26 @@
 package app.messages;
 
+import java.util.Arrays;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration // 빈을 정의하기 위한 것임을 스프링에 알려주기 위해
 @ComponentScan("app.messages") // 컴포넌트를 스캔할 기본 패키지를 알려주기 위해
 public class AppConfig {
 
+  // 스프링부트 애플리케이션에서 필터를 등록하려면 FilterRegistrationBean을 만들어 AppConfig에 등록한다.
+  // setFilter 메서드로 Filter 설정, setOrder 메서드로 이 필터를 체인 내에 배치 (이때 순서값이 작은 것이 앞에 위치)
+  // setUrlPatterns 메서드는 Filter를 등록할 구체적 경로를 지정 (이 필터는 /messages로 시작하는 요청만 처리)
+  @Bean
+  public FilterRegistrationBean<AuditingFilter> auditingFilterRegistrationBean() {
+    FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
+    AuditingFilter filter = new AuditingFilter();
+    registration.setFilter(filter);
+    registration.setOrder(Integer.MAX_VALUE);
+    registration.setUrlPatterns(Arrays.asList("/messages/*"));
+    return registration;
+  }
 }
---
+++ backend/src/main/java/app/messages/AuditingFilter.java
@@ -0,0 +1,27 @@
+package app.messages;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+// 필터는 자바 EE의 기술 중에 하나이며, 디자인 패턴인 책임 연쇄 패턴을 구현한 것,
+// 서블릿에 도달하기 전에 HTTP 요청에 대한 필터링 작업을 수행할 때 유용하다.
+// 경과 시간을 계산하고 디버그 모드로 로그를 출력하는 필터 
+public class AuditingFilter extends GenericFilterBean {
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+    long start = new Date().getTime();
+    chain.doFilter(req, res);
+    long elapsed = new Date().getTime() - start;
+    HttpServletRequest request = (HttpServletRequest) req;
+    logger.info("Request[uri=" + request.getRequestURI() + ", method=" + request.getMethod() + "] completed in "
+        + elapsed + " ms");
+  }
+}

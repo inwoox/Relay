187a95046864e43faee63b5ac51239e4f3d02a46
seco <inwoox@gmail.com>
2020-11-26 15:02:30 +0900

    -------------------------------------------------------------------------------- 백엔드 뼈대 생성 / STS에서 스프링 이니셜라이저를 통해 스프링부트 프로젝트 생성 (적절한 의존성 추가 및 DataSource 설정 / docker로 MySQL 실행)

---
+++ taskagile/src/main/java/taskagile/TaskagileApplication.java
@@ -0,0 +1,13 @@
+package taskagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskagileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskagileApplication.class, args);
+	}
+
+}

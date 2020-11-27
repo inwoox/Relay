9fba922a0c160a17d7f9b4f264237d3ed91f62fb
seco <inwoox@gmail.com>
2020-11-27 12:28:40 +0900

    [부트스트랩] 부트스트랩 사용 - 웹팩을 설정 (vue.config.js에 설정 변경) 해서 부트스트랩의 스타일을 별도의 .css 파일로 관리하는 형태로 부트스트랩을 적용한다. /  [NPM] npm install 명령 시에 --save 명령어를 입력하면 모듈을 추가한 프로젝트의 package.json에 "dependencies"로 해당 모듈을 추가 / --save-dev 명령어를 입력하면, package.json에 "devDependencies"로 해당 모듈을 추가 / 이후 다른 프로젝트에서 해당 package.json을 가지고, npm install을 실행하면, dependencies, devDependencies 모든 모듈을 설치 / dependencies만 설치하려면 npm install --production

--- taskagile/frontend/vue.config.js
+++ taskagile/frontend/vue.config.js
@@ -6,5 +6,15 @@ module.exports = {
 				target: 'http://localhost:8080'
 			}
 		}
-	}
+  },
+  // 부트스트랩을 사용하려면, 부트스트랩의 컴파일된 CSS를 애플리케이션으로 가져와야한다. 여기서는 웹팩을 이용해 부트스트랩의 CSS를 가져온다.
+  // 웹팩에 style이라는 새로운 진입점을 만들어, 모든 서드 파티 스타일을 하나의 .css 파일로 묶는다.
+  configureWebpack: {
+    entry: {
+      app: './src/main.js',
+      style: [
+        'bootstrap/dist/css/bootstrap.min.css' // 필요할 때마다 서드 파티 라이브러리의 스타일을 이곳에 추가한다.
+      ]
+    }
+  }
 }

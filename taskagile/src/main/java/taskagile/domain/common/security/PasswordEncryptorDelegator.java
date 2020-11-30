84819a220430e0302333285c5ca72db23e5c4b9b
seco <inwoox@gmail.com>
2020-11-30 16:16:09 +0900

    [정리] [도메인 모델 (코어)] : 애플리케이션 서비스 / 도메인 서비스 / 인프라 서비스   ///   [인프라스트럭처] :  외부 서비스를 호출하는 구현체   ///  [요청 진행 흐름] (컨트롤러 클래스 : 인바운드 어댑터) 에서  (애플리케이션 서비스 : 인바운드 포트)를 호출하여, 비즈니스 로직에 요청  ///   비즈니스 로직에 들어온 요청을 (도메인 서비스 또는 인프라 서비스 : 아웃바운드 포트 / 예: Repo 인터페이스) 를 통해  (인프라스트럭처 : 아웃바운드 어댑터 / 인터페이스 구현체)를 거쳐, 예컨대 DB (외부 서비스)를 호출하여 처리   ///   [도메인 -> 인프라 영역으로는 접근 가능, 반대로는 불가능해야한다. / 무조건 포트, 어댑터를 통해서만 도메인 -> 인프라 접근]

--- taskagile/src/main/java/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ taskagile/src/main/java/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -2,6 +2,7 @@ package taskagile.domain.common.security;
 
 import org.springframework.stereotype.Component;
 
+// 실제 비밀 번호 암호화 로직이 다른 곳으로 위임됨을 나타낸다 (스프링 시큐리티의 PasswordEncoder로 암호화 로직이 위임된다)
 @Component
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
 
--- taskagile/src/main/java/taskagile/domain/model/user/UserRepository.java
+++ taskagile/src/main/java/taskagile/domain/model/user/UserRepository.java
@@ -1,31 +1,12 @@
 package taskagile.domain.model.user;
 
-/**
- * User repository interface
- */
+
+// 스프링 데이터 JPA를 사용하기 위해 @Repository / extends JpaRepository<User, Long>을 붙이면, 
+// 스프링 데이터 JPA가 자동으로 인터페이스 구현체 (하이버네이트 구현체)를 생성하게 할 수 있다. / 하지만 리포지토리 인터페이스가 깔끔하기를 원하고, 
+// 마이바티스 , 스프링 JDBC 같은 다른 구현체로의 전환이 자유롭고 싶다면 스프링 데이터 JPA를 사용하지 않고, 리포지토리를 그대로 유지한
+// 그리고 인프라와 관련된 구현을 도메인 모델로부터 분리하기 위해, 이 인터페이스의 구현은 infrastructure.repository 패키지에서 구현한다.
 public interface UserRepository {
-
-  /**
-   * Find user by a username
-   *
-   * @param username the provided username used for finding user
-   * @return an instance of <code>User</code> if found, null otherwise
-   */
   User findByUsername(String username);
-
-  /**
-   * Find user by an email address
-   *
-   * @param emailAddress the provided email address used for finding user
-   * @return an instance of <code>User</code> if found, null otherwise
-   */
   User findByEmailAddress(String emailAddress);
-
-  /**
-   * Save a new user or an existing user
-   *
-   * @param user the user instance to be saved
-   * @return the newly save user
-   */
   void save(User user);
 }

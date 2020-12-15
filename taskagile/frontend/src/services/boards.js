d35920e29aa08e6f8cce461d6884a1757bf171bf
seco <inwoox@gmail.com>
2020-12-15 18:45:51 +0900

    [프론트엔드 구현] Vuex (상태 관리) / View (화면) / Vue-router (View 경로) / View에서 쓰는 Service (API 호출 원형), Modal, Component, Util / main.js (전역 설정)

--- taskagile/frontend/src/services/boards.js
+++ taskagile/frontend/src/services/boards.js
@@ -10,5 +10,23 @@ export default {
         reject(errorParser.parse(error))
       })
     })
+  },
+  addMember (boardId, usernameOrEmailAddress) {
+    return new Promise((resolve, reject) => {
+      axios.post('/boards/' + boardId + '/members', { usernameOrEmailAddress }).then(({data}) => {
+        resolve(data)
+      }).catch((error) => {
+        reject(errorParser.parse(error))
+      })
+    })
+  },
+  getBoard (boardId) {
+    return new Promise((resolve, reject) => {
+      axios.get('/boards/' + boardId).then(({data}) => {
+        resolve(data)
+      }).catch((error) => {
+        reject(errorParser.parse(error))
+      })
+    })
   }
 }
